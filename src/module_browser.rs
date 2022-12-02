53952471888d8a4acb7d8b3622c3b85c9ff21fce
Przemyslaw Hugh Kaznowski <hughkaznowski@protonmail.com>
2022-12-02 21:44:03 +0000

    Handling errors

--- src/module_browser.rs
+++ src/module_browser.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::io::Error;
+
 #[derive(Debug)]
 pub struct LearningModule<'a> {
     metadata: LearningModuleMetadata<'a>,
@@ -24,17 +27,22 @@ pub struct Version {
 pub struct LearningModuleEntry {
 }
 
-pub fn list_modules(directory: &str) -> Vec<LearningModule> {
-    return vec![
-        LearningModule{
-            metadata: LearningModuleMetadata{
-                name: "name one",
-                author: "Hugh",
-                updated_date: "this is a date",
-                file_version: Version{major: 1, minor: 0, patch: 0},
-                format_version: Version{major: 1, minor: 0, patch: 0},
+pub fn list_modules(directory: &str) -> Result<Vec<LearningModule>, Error> {
+    let paths = fs::read_dir(directory)?;
+    let mut ret = Vec::new();
+    for path in paths {
+        ret.push(
+            LearningModule{
+                metadata: LearningModuleMetadata{
+                    name: "name one",
+                    author: "Hugh",
+                    updated_date: "this is a date",
+                    file_version: Version{major: 1, minor: 0, patch: 0},
+                    format_version: Version{major: 1, minor: 0, patch: 0},
+                },
+                entries: vec![],
             },
-            entries: vec![],
-        },
-    ]
+        )
+    }
+    return Ok(ret)
 }
